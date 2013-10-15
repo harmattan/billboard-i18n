3b031b22371d7838c8115f51962fa2a357f34888
Lorenzo Facca <lorenzo.facca@gmail.com>
2013-10-15 23:26:52 +0200

    Added Italian translation

---
+++ it.ts
@@ -0,0 +1,499 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="it_IT">
+<context>
+    <name>billboard-ui</name>
+    <message>
+        <location filename="../billboard-ui.qml" line="28"/>
+        <source>General</source>
+        <translation>Generale</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="32"/>
+        <source>Enable Billboard</source>
+        <translation>Attiva Billboard</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="36"/>
+        <source>Content</source>
+        <translation>Contenuto</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="55"/>
+        <source>Insert</source>
+        <translation>Inserisci</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="67"/>
+        <source>Presets</source>
+        <translation>Preimpostati</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="96"/>
+        <source>Preview</source>
+        <translation>Anteprima</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="109"/>
+        <source>Appearance</source>
+        <translation>Aspetto</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="113"/>
+        <source>Show other logos</source>
+        <translation>mostra altri loghi</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="119"/>
+        <location filename="../billboard-ui.qml" line="235"/>
+        <source>Text color</source>
+        <translation>Colore del testo</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="126"/>
+        <location filename="../billboard-ui.qml" line="256"/>
+        <source>Text size</source>
+        <translation>Dimensione del testo</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="133"/>
+        <location filename="../billboard-ui.qml" line="277"/>
+        <source>Font family</source>
+        <translation>Carattere del testo</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="140"/>
+        <location filename="../billboard-ui.qml" line="294"/>
+        <source>Text alignment</source>
+        <translation>Allineamento testo</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="152"/>
+        <source>Insert new field</source>
+        <translation>Inserisci nuovo campo</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="157"/>
+        <source>Compact date</source>
+        <translation>Data compatta</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="158"/>
+        <source>Full date</source>
+        <translation>Data completa</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="160"/>
+        <source>Weekday name</source>
+        <translation>Giorno della settimana</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="161"/>
+        <source>Day of month</source>
+        <translation>Giorno del mese</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="162"/>
+        <source>Month name</source>
+        <translation>Nome del mese</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="163"/>
+        <source>Month</source>
+        <translation>Numero del mese</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="164"/>
+        <source>Year</source>
+        <translation>Anno</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="165"/>
+        <source>Week number</source>
+        <translation>Numero della settimana</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="167"/>
+        <source>Song artist</source>
+        <translation>Artista del brano</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="168"/>
+        <source>Song title</source>
+        <translation>Titolo del brano</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="169"/>
+        <source>Song &quot;Artist - Title&quot;</source>
+        <translation>Brano &quot;Artista - Titolo&quot;</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="174"/>
+        <source>Cell network name</source>
+        <translation>Nome network tel</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="175"/>
+        <source>Internet connection</source>
+        <translation>Connessione internet</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="176"/>
+        <source>IP address</source>
+        <translation>Indirizzo IP</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="178"/>
+        <source>Battery charge (text)</source>
+        <translation>Carica batteria (numero)</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="179"/>
+        <source>Battery charge (bar)</source>
+        <translation>Carica batteria (barra)</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="180"/>
+        <source>Battery charge (icon)</source>
+        <translation>Carica batteria (icona)</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="181"/>
+        <source>Colored battery bar</source>
+        <translation>Barra colorata batteria</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="182"/>
+        <source>Active profile</source>
+        <translation>Profilo attivo</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="184"/>
+        <source>Upcoming events</source>
+        <translation>Prossimi eventi</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="185"/>
+        <source>Today&apos;s events</source>
+        <translation>Eventi di oggi</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="187"/>
+        <source>MeeCast station</source>
+        <translation>Stazione meteo MeeCast</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="188"/>
+        <source>MeeCast temperature</source>
+        <translation>Temperatura MeeCast</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="189"/>
+        <source>MeeCast description</source>
+        <translation>Descrizione MeeCast</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="190"/>
+        <source>MeeCast icon</source>
+        <translation>Icona MeeCast</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="192"/>
+        <source>IM presence status</source>
+        <translation>Presenza in servizi di chat</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="193"/>
+        <source>Wazapp online status</source>
+        <translation>Stato connessione Wazapp</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="195"/>
+        <source>USSD Balance</source>
+        <translation>USSD Balance</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="196"/>
+        <source>USSD Balance (2nd line)</source>
+        <translation>USSD Balance (1a linea)</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="197"/>
+        <source>USSD Balance (3rd line)</source>
+        <translation>USSD Balance (2a linea)</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="199"/>
+        <source>Brackets: {}</source>
+        <translation>Parentesi: {}</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="200"/>
+        <source>Brackets: {{}}</source>
+        <translation>Parentesi: {{}}</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="201"/>
+        <source>Embed image: &lt;&lt;&gt;&gt;</source>
+        <translation>Inserisci immagine: &lt;&lt;&gt;&gt;</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="202"/>
+        <source>Embed image (dithered): &lt;&lt;#&gt;&gt;</source>
+        <translation>Inserisci immagine (dithered): &lt;&lt;#&gt;&gt;</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="216"/>
+        <source>Select preset</source>
+        <translation>Seleziona preimpostato</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="221"/>
+        <source>Default</source>
+        <translation>Impostazione defaulf</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="222"/>
+        <source>Empty</source>
+        <translation>Vuoto</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="223"/>
+        <source>Default with song</source>
+        <translation>Default con canzone</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="224"/>
+        <source>Full device status</source>
+        <translation>Stato completo del dispositivo</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="225"/>
+        <source>Date, battery and song (icons)</source>
+        <translation>Data, batteria e brano (icone)</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="240"/>
+        <source>White</source>
+        <translation>Bianco</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="241"/>
+        <source>Red</source>
+        <translation>Rosso</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="242"/>
+        <source>Green</source>
+        <translation>Verde</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="243"/>
+        <source>Blue</source>
+        <translation>Blu</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="244"/>
+        <source>Yellow</source>
+        <translation>Giallo</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="245"/>
+        <source>Cyan</source>
+        <translation>Ciano</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="246"/>
+        <source>Magenta</source>
+        <translation>Magenta</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="261"/>
+        <source>Smaller</source>
+        <translation>Molto piccolo</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="262"/>
+        <source>Small</source>
+        <translation>Piccolo</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="263"/>
+        <source>Normal</source>
+        <translation>Normale</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="264"/>
+        <source>Big</source>
+        <translation>Grande</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="265"/>
+        <source>Bigger</source>
+        <translation>Più grande</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="266"/>
+        <source>Very big</source>
+        <translation>Molto grande</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="267"/>
+        <source>Crazy big</source>
+        <translation>Enorme</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="282"/>
+        <source>Pure</source>
+        <translation>Pure</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="283"/>
+        <source>Sans</source>
+        <translation>Sans</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="284"/>
+        <source>Monospace</source>
+        <translation>Monospace</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="299"/>
+        <source>Left</source>
+        <translation>Sinistra</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="300"/>
+        <source>Right</source>
+        <translation>Destra</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="301"/>
+        <source>Center</source>
+        <translation>Al centro</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="313"/>
+        <source>Get more apps</source>
+        <translation>Scarica altre app</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="321"/>
+        <source>Help / About</source>
+        <translation>Crediti &amp; Aiuto</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="375"/>
+        <source>Show useful information in the low power mode screen.</source>
+        <translation>Mostra informazioni utili nella schermata di standby.</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="377"/>
+        <source>Syntax information:</source>
+        <translation>Info sulla sintassi:</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="378"/>
+        <source>Insert property</source>
+        <translation>Inserisci struttura</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="379"/>
+        <source>Insert &quot;text&quot; when property is not empty</source>
+        <translation>Inserisci &quot;testo&quot; quando la struttura non è vuota</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="380"/>
+        <source>Insert &quot;text&quot; when property is empty</source>
+        <translation>Inserisci &quot;testo&quot; quando la struttura è vuota</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="382"/>
+        <source>Examples:</source>
+        <translation>Esempi:</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="383"/>
+        <source>No song</source>
+        <translation>Nessun brano</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="383"/>
+        <source>Insert &quot;No song&quot; when no song is selected</source>
+        <translation>Inserisci &quot;Nessun brano&quot; quando è ferma la riproduzione</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="384"/>
+        <source>Insert the current date (short format)</source>
+        <translation>Inserisci la data di oggi (formato breve)</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="386"/>
+        <source>Advanced features:</source>
+        <translation>Funzioni avanzate:</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="387"/>
+        <source>Insert output of &quot;uptime&quot; command</source>
+        <translation>Inserisci output di &quot;uptime&quot;</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="388"/>
+        <source>Insert output of &quot;/home/user/test.py&quot; script</source>
+        <translation>Inserisci output dello &quot;/home/user/test.py&quot; script</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="389"/>
+        <source>Use a color name (%1) to set the inline color</source>
+        <translation>Usare il nome di un colore (%1) per impostare il colore nella linea</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="390"/>
+        <source>Reset the inline color to the default color</source>
+        <translation>Reset colori nelle linee al colore di default</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="391"/>
+        <source>Insert image (normal)</source>
+        <translation>Inserisci immagine (normale)</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="392"/>
+        <source>Insert image (with dithering)</source>
+        <translation>Inserisci immagine (dithering)</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="394"/>
+        <source>Author: %1</source>
+        <translation>Autore: %1</translation>
+    </message>
+    <message>
+        <location filename="../billboard-ui.qml" line="396"/>
+        <source>For support, please contact %1</source>
+        <translation>Per supporto contatta %1</translation>
+    </message>
+</context>
+<context>
+    <name>store-description</name>
+    <message>
+        <location filename="../store-description.qml" line="7"/>
+        <source>Billboard Standby Screen</source>
+        <translation>Billboard Standby Screen</translation>
+    </message>
+    <message>
+        <location filename="../store-description.qml" line="14"/>
+        <source>Display the current date, battery level, current song, internet connection information and more on your N9&apos;s standby screen - easy customization!</source>
+        <translation>Mostra sullo stand-by screen: data, carica della batteria, canzone in riproduzione, info sulla connessione e molto altro, con una semplice configurazione!</translation>
+    </message>
+    <message>
+        <location filename="../store-description.qml" line="15"/>
+        <source>After the initial installation, you have to restart your N9 for configuration changes to take effect.</source>
+        <translation>Dopo la prima installazione, devi riavviare N9 per fare si che la configurazione venga caricata.</translation>
+    </message>
+    <message>
+        <location filename="../store-description.qml" line="16"/>
+        <source>Problems? Please e-mail %1 - we&apos;re here to help!</source>
+        <translation>Qualche problema? Contatta %1 siamo quì per aiutarti!</translation>
+    </message>
+</context>
+</TS>
